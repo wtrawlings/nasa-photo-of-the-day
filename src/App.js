dc57ce873d6c2475775820c1c286bd0c27e56ba1
Warren Rawlings <rawlings.warren@gmail.com>
2019-10-24 22:35:24 -0400

    added axios and nasa key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
+function photoData() {
+  axios
+    .get(
+      "https://api.nasa.gov/planetary/apod?api_key=l8YJy4uVBHiBDOGzF6f4BuJi65Oe199Ow1cov24N"
+    )
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+photoData();
 function App() {
+  const [date, setDate] = useState();
+
   return (
     <div className="App">
       <p>
