e1d75fbb7e184157f2650c60afcde0321a31100f
wtrawlings <rawlings.warren@gmail.com>
2020-01-11 21:22:38 -0500

    more css changes over weekend

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const DescriptionP = styled.p`
   color: orange;
   background-color: black;
-  
+  border-radius: 50%;
   shape-outside: circle(50%);
   width: 600px;
   height: 600px;
